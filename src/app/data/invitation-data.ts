3785d7d8a84fce226ac342bab0112ee9e0f574d1
EduardoVillafuerte <eduardovillafuerte@udla.edu.ec>
2026-07-26 19:22:31 -0500

    commit

--- src/app/data/invitation-data.ts
+++ src/app/data/invitation-data.ts
@@ -6,14 +6,14 @@ export const INVITATION_DATA: InvitationData = {
   date: 'Sábado, 1 de Agosto 2026',
   time: '7:00 PM',
   venue: 'Mejia',
-  dressCode: 'Formal / Elegante',
-  message: 'Con inmensa alegría te invito a celebrar este logro tan importante en mi vida. Tu presencia hará de este día algo inolvidable.',
+  dressCode: 'Formal',
+  message: 'Con inmensa alegría te espero para celebrar este logro tan importante en mi vida. Tu presencia hará de este día algo inolvidable.',
   whatsappGroupUrl: 'https://chat.whatsapp.com/LjFLzETfab36jyKpKIXBhq?s=sh&p=i&mlu=0',
   whatsappMessage: 'Confirmo mi asistencia!!',
   googleFormUrl: '',
   galleryImages: [
     'assets/images/Gallery/Foto2.jpg',
     'assets/images/Gallery/Foto3.png',  
-    'assets/images/Gallery/Foto1.jpeg'
+    'assets/images/Gallery/Foto1.jpg'
   ]
 };
